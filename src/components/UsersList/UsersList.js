07ce018e8359ca880cbd6cf0c0b26d76e42298c6
Michał Foltyn <kontakt@my-site.pl>
2021-12-29 16:35:35 +0100

    style usersList component with scss module

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { users } from 'data/users';
-import UsersListItem from 'components/UsersListItem/UserListItem';
+import UsersListItem from 'components/UsersListItem/UsersListItem';
+
+import styles from 'assets/GlobalStyles.module.scss';
+import containerStyles from './UsersList.module.scss';
 
 const UsersList = () => {
   return (
-    <div>
+    <div
+      className={`${containerStyles.container} ${
+        false ? containerStyles.blackBorder : containerStyles.redBorder
+      }`}
+    >
+      <div className={styles.box}></div>
       <ul>
         {users.map((userData) => (
           <UsersListItem userData={userData} />
--- src/components/UsersListItem/UsersListItem.js
+++ src/components/UsersListItem/UsersListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './UsersListItem.module.scss';
+import globalStyles from 'assets/GlobalStyles.module.scss';
+
 const UsersListItem = ({ userData: { name, average, attendance = '0%' } }) => {
   return (
-    <li>
+    <li className={styles.container}>
+      <div className={globalStyles.box}></div>
       <div>{average}</div>
       <div>
         <p>{name}</p>
